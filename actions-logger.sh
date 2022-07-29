245fb17e0bcdf4c394d68e392765d4efdbbbb532
Ace Sobremont <100104970+xqwtxon@users.noreply.github.com>
2022-07-29 17:45:50 +0800

    Fix

--- actions-logger.sh
+++ actions-logger.sh
@@ -1,12 +1,12 @@
-if [[ -e "actions.log" ]]; 
+if [ -e "actions.log" ]; 
 then
    rm "actions.log";
 fi
 echo =================================================================== > "actions.log"
-echo As of $(date +"%d-%m-%Y %T.%N %Z") report data from actions > "actions.log"
+echo As of $(date +"%d-%m-%Y %T.%N") report data from actions > "actions.log"
 echo "" > "actions.log"
 echo Action Exit Code: $? > "actions.log"
-echo Time Success: $(date +%T.%N %Z) > "actions.log"
+echo Time Success: $(date +%T.%N) > "actions.log"
 echo =================================================================== > "actions.log"
 echo User Agent Information (Hosted) > "actions.log"
 echo Kernel Name: $(uname -s) > "actions.log"
@@ -18,11 +18,11 @@ echo Processor: $(uname -p) > "actions.log"
 echo Hardware Platform: $(uname -i) > "actions.log"
 echo Operating System: $(uname -o) > "actions.log"
 echo =================================================================== > "actions.log"
-echo As of $(date +"%d-%m-%Y), Here's what files are included in actions.
+echo As of $(date +"%d-%m-%Y"), What files are included in actions? > "actions.log"
 if ! command -v tree &> /dev/null
 then
     echo "tree is not installed. Installing..."
     sudo apt-get install tree
     echo "tree command is installed."
 fi
-tree > "actions.log"
+tree --charset X > "actions.log"
