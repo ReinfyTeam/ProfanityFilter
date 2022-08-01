6a11dc18376ecb49ef4dfd205670a1ddc525761c
Ace Sobremont <100104970+xqwtxon@users.noreply.github.com>
2022-08-01 18:52:04 +0800

    Fix
    [Skip Actions]

--- actions-logger.sh
+++ actions-logger.sh
@@ -1,20 +1,24 @@
-if [ -e "actions.log" ]; 
-then
-   rm "actions.log";
+#!/bin/env bash
+
+log="logs/actions_$(date +%d_%m_%Y_%T).log"
+
+if [[ ! -e "logs/" ]]; then
+    mkdir $dir
 fi
-echo =================================================================== > "actions.log"
-echo As of $(date +"%d-%m-%Y %T.%N") report data from actions > "actions.log"
+
+echo "===================================================================" >> $log
+echo "As of $(date +%d-%m-%Y %T.%N) report data from actions" >> $log
 echo "" > "actions.log"
-echo Action Exit Code: $? > "actions.log"
-echo Time Success: $(date +%T.%N) > "actions.log"
-echo =================================================================== > "actions.log"
-echo Github User Agent Information [Hosted] > "actions.log"
-echo Kernel Name: $(uname -s) > "actions.log"
-echo Kernel Release: $(uname -r) > "actions.log"
-echo Kernel Version: $(uname -v) > "actions.log"
-echo Nodename: $(uname -n) > "actions.log"
-echo Machine: $(uname -m) > "actions.log"
-echo Processor: $(uname -p) > "actions.log"
-echo Hardware Platform: $(uname -i) > "actions.log"
-echo Operating System: $(uname -o) > "actions.log"
-echo =================================================================== > "actions.log"
+echo "Action Exit Code: $?" >> $log
+echo "Time Success: $(date +%T.%N)" >> $log
+echo "=================================================================== >> $log
+echo "Github User Agent Information [Hosted] >> $log
+echo "Kernel Name: $(uname -s)" >> $log
+echo "Kernel Release: $(uname -r)" >> "actions.log"
+echo "Kernel Version: $(uname -v)" >> $log
+echo "Nodename: $(uname -n)" >> $log
+echo "Machine: $(uname -m)" >> $log
+echo "Processor: $(uname -p)" >> $log
+echo "Hardware Platform: $(uname -i)" >> $log
+echo "Operating System: $(uname -o)" >> $log
+echo "===================================================================" >> $log
