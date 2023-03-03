d34b11cfa4b395a339ab3988f343b51aaffa323b
Ace Sobremont <xqwtxon@hotmail.com>
2023-03-03 13:17:03 +0800

    Fix $this

--- src/Utils/PluginUtils.php
+++ src/Utils/PluginUtils.php
@@ -80,7 +80,7 @@ final class PluginUtils {
 	 * Format Message. Dont call it directly.
 	 */
 	public static function formatMessage(string $message, ?Player $player = null) : string { // TODO: Move this in the event class
-		$message = str_replace("{type}", $this->getConfig()->get("punishment-type") . "ed", $message);
+		$message = str_replace("{type}", Loader::getInstance()->getConfig()->get("punishment-type") . "ed", $message);
 
 		// FOR PLAYERS
 		if ($player === null) {
