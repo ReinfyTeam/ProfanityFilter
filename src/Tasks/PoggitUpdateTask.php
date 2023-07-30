eec9c6b47c255882bc6670bfb5aa0eb6a0996694
xqwtxon <lawlesshen10097@outlook.com>
2023-07-30 10:13:28 +0800

    Fixed github files...

---
+++ src/Tasks/PoggitUpdateTask.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ *
+ *  ____           _            __           _____
+ * |  _ \    ___  (_)  _ __    / _|  _   _  |_   _|   ___    __ _   _ __ ___
+ * | |_) |  / _ \ | | | '_ \  | |_  | | | |   | |    / _ \  / _` | | '_ ` _ \
+ * |  _ <  |  __/ | | | | | | |  _| | |_| |   | |   |  __/ | (_| | | | | | | |
+ * |_| \_\  \___| |_| |_| |_| |_|    \__, |   |_|    \___|  \__,_| |_| |_| |_|
+ *                                   |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author ReinfyTeam
+ * @link https://github.com/ReinfyTeam/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace ReinfyTeam\ProfanityFilter\Tasks;
+
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+use pocketmine\utils\Internet;
+use ReinfyTeam\ProfanityFilter\Loader;
+use ReinfyTeam\ProfanityFilter\Utils\Language;
+use function json_decode;
+use function version_compare;
+use function vsprintf;
+
+class PoggitUpdateTask extends AsyncTask {
+	private const POGGIT_RELEASES_URL = "https://poggit.pmmp.io/releases.min.json?name=";
+
+	public function __construct(private string $pluginName, private string $pluginVersion) {
+		//NOOP
+	}
+
+	public function onRun() : void {
+		$json = Internet::getURL(self::POGGIT_RELEASES_URL . $this->pluginName, 10, [], $err);
+		$highestVersion = $this->pluginVersion;
+		$artifactUrl = "";
+		$api = "";
+		if ($json !== null) {
+			$releases = json_decode($json->getBody(), true);
+			if ($releases === null) {
+				return;
+			} // Issue Fix: https://github.com/ReinfyTeam/ProfanityFilter/issues/107
+			foreach ($releases as $release) {
+				if (version_compare($highestVersion, $release["version"], ">=")) {
+					continue;
+				}
+				$highestVersion = $release["version"];
+				$artifactUrl = $release["artifact_url"];
+				$api_from = $release["api"][0]["from"];
+				$api_to = $release["api"][0]["to"];
+			}
+		}
+
+		$this->setResult([$highestVersion, $artifactUrl, $api, $err]);
+	}
+
+	public function onCompletion() : void {
+		$lang = new Language();
+		$plugin = Server::getInstance()->getPluginManager()->getPlugin($this->pluginName);
+		if ($plugin === null) {
+			return;
+		}
+		[$highestVersion, $artifactUrl, $api, $err] = $this->getResult();
+		if ($highestVersion === null || $artifactUrl === null || $api === null) {
+			Server::getInstance()->getLogger()->critical($lang->translateMessage("new-update-prefix") . " " . vsprintf($lang->translateMessage("update-error"), ["Trying to update on github..."]));
+			$plugin->getServer()->getAsyncPool()->submitTask(new GithubUpdateTask(Loader::getInstance()->getDescription()->getName(), Loader::getInstance()->getDescription()->getVersion()));
+			return;
+		} // Issue: https://github.com/ReinfyTeam/ProfanityFilter/issues/107
+		if ($err !== null) {
+			Server::getInstance()->getLogger()->critical($lang->translateMessage("new-update-prefix") . " " . vsprintf($lang->translateMessage("update-error"), [$err]));
+			Server::getInstance()->getLogger()->notice($lang->translateMessage("new-update-prefix") . " " . $lang->translateMessage("update-retry"));
+			$plugin->getServer()->getAsyncPool()->submitTask(new GithubUpdateTask(Loader::getInstance()->getDescription()->getName(), Loader::getInstance()->getDescription()->getVersion()));
+			return;
+		}
+
+		if ($highestVersion !== $this->pluginVersion) {
+			Server::getInstance()->getLogger()->warning($lang->translateMessage("new-update-prefix") . " " . vsprintf($lang->translateMessage("new-update-found"), [$highestVersion, $api]));
+			Server::getInstance()->getLogger()->warning($lang->translateMessage("new-update-prefix") . " " . vsprintf($lang->translateMessage("new-update-details"), [$api_from, $api_to]));
+			Server::getInstance()->getLogger()->warning($lang->translateMessage("new-update-prefix") . " " . vsprintf($lang->translateMessage("new-update-download"), [$artifactUrl]));
+		} else {
+			Server::getInstance()->getLogger()->notice($lang->translateMessage("new-update-prefix") . " " . $lang->translateMessage("no-updates-found"));
+		}
+	}
+}
---
+++ src/Utils/PluginUtils.php
@@ -0,0 +1,170 @@
+<?php
+
+/*
+ *
+ *  ____           _            __           _____
+ * |  _ \    ___  (_)  _ __    / _|  _   _  |_   _|   ___    __ _   _ __ ___
+ * | |_) |  / _ \ | | | '_ \  | |_  | | | |   | |    / _ \  / _` | | '_ ` _ \
+ * |  _ <  |  __/ | | | | | | |  _| | |_| |   | |   |  __/ | (_| | | | | | | |
+ * |_| \_\  \___| |_| |_| |_| |_|    \__, |   |_|    \___|  \__,_| |_| |_| |_|
+ *                                   |___/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author ReinfyTeam
+ * @link https://github.com/ReinfyTeam/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace ReinfyTeam\ProfanityFilter\Utils;
+
+use DateInterval;
+use DateTime;
+use pocketmine\player\Player;
+use pocketmine\utils\TextFormat;
+use ReinfyTeam\ProfanityFilter\Loader;
+use function array_keys;
+use function array_values;
+use function count;
+use function is_bool;
+use function ltrim;
+use function preg_match_all;
+use function preg_replace;
+use function str_replace;
+use function strlen;
+use function strtoupper;
+use function strval;
+use function substr;
+use function trim;
+
+final class PluginUtils {
+	/**
+	 * Colorise Messages turns & to § and etc.
+	 */
+	public static function colorize(string $message) : string {
+		$replacements = [
+			"&" => "§",
+			"{BLACK}" => TextFormat::BLACK,
+			"{DARK_BLUE}" => TextFormat::DARK_BLUE,
+			"{DARK_GREEN}" => TextFormat::DARK_GREEN,
+			"{DARK_AQUA}" => TextFormat::DARK_AQUA,
+			"{DARK_RED}" => TextFormat::DARK_RED,
+			"{DARK_PURPLE}" => TextFormat::DARK_PURPLE,
+			"{GOLD}" => TextFormat::GOLD,
+			"{GRAY}" => TextFormat::GRAY,
+			"{DARK_GRAY}" => TextFormat::DARK_GRAY,
+			"{BLUE}" => TextFormat::BLUE,
+			"{GREEN}" => TextFormat::GREEN,
+			"{AQUA}" => TextFormat::AQUA,
+			"{RED}" => TextFormat::RED,
+			"{LIGHT_PURPLE}" => TextFormat::LIGHT_PURPLE,
+			"{YELLOW}" => TextFormat::YELLOW,
+			"{WHITE}" => TextFormat::WHITE,
+			"{OBFUSCATED}" => TextFormat::OBFUSCATED,
+			"{BOLD}" => TextFormat::BOLD,
+			"{STRIKETHROUGH}" => TextFormat::STRIKETHROUGH,
+			"{UNDERLINE}" => TextFormat::UNDERLINE,
+			"{ITALIC}" => TextFormat::ITALIC,
+			"{RESET}" => TextFormat::RESET,
+		];
+		$message = str_replace(array_keys($replacements), array_values($replacements), $message);
+		return $message;
+	}
+	/**
+	 * Format Message. Dont call it directly.
+	 */
+	public static function formatMessage(string $message, ?Player $player = null) : string { // TODO: Move this in the event class
+		$message = str_replace("{type}", Loader::getInstance()->getConfig()->get("punishment-type") . "ed", $message);
+
+		// FOR PLAYERS
+		if ($player === null) {
+			return $message;
+		}
+		$message = str_replace("{player_name}", "Unknown", $message);
+		$message = str_replace("{player_name}", $player->getName(), $message);
+		$message = str_replace("{player_ping}", strval($player->getNetworkSession()->getPing()), $message);
+
+		return $message;
+	}
+
+	public static function format(string $message, array $options, array $value) : string {
+		foreach ($value as $v) {
+			foreach ($options as $o) {
+				$message = str_replace($o, $v, $message);
+			}
+		}
+		return $message;
+	}
+
+	public static function assumeNotFalse(mixed $given, string $message = "This line should be not false. PLEASE REPORT THIS TO THE DEVELOPER.", bool $invert = false) {
+		if (is_bool($given)) {
+			if (!$given) {
+				throw new \RuntimeException($message); // assume not false ;(
+			}
+		}
+	}
+
+	/**
+	 * Convert String to Timestamp
+	 *
+	 * @return ?array
+	 */
+	private static function stringToTimestamp(string $string) : ?array {
+		/**
+		 * Rules:
+		 * Integers without suffix are considered as seconds
+		 * "s" is for seconds
+		 * "m" is for minutes
+		 * "h" is for hours
+		 * "d" is for days
+		 * "w" is for weeks
+		 * "mo" is for months
+		 * "y" is for years
+		 */
+		if (trim($string) === "") {
+			return null;
+		}
+		$t = new DateTime();
+		preg_match_all("/[0-9]+(y|mo|w|d|h|m|s)|[0-9]+/", $string, $found);
+		if (count($found[0]) < 1) {
+			return null;
+		}
+		$found[2] = preg_replace("/[^0-9]/", "", $found[0]);
+		foreach ($found[2] as $k => $i) {
+			switch ($c = $found[1][$k]) {
+				case "y":
+				case "w":
+				case "d":
+					$t->add(new DateInterval("P" . $i . strtoupper($c)));
+					break;
+				case "mo":
+					$t->add(new DateInterval("P" . $i . strtoupper(substr($c, 0, strlen($c) - 1))));
+					break;
+				case "h":
+				case "m":
+				case "s":
+					$t->add(new DateInterval("PT" . $i . strtoupper($c)));
+					break;
+				default:
+					$t->add(new DateInterval("PT" . $i . "S"));
+					break;
+			}
+			$string = str_replace($found[0][$k], "", $string);
+		}
+		return [$t, ltrim(str_replace($found[0], "", $string))];
+	}
+
+	public static function getDuration() {
+		if (Loader::getInstance()->getConfig()->get("ban-duration") === "Forever") {
+			return null;
+		} else {
+			return self::stringToTimestamp(Loader::getInstance()->getConfig()->get("ban-duration"));
+		}
+	}
+}
